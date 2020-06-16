f7478a5386e4e7be5c129ab17fc3547620b028bd
syaofox <syaofox@users.noreply.github.com>
2020-06-16 18:41:07 +0800

    modify site 2020年06月16日, 周二 18:41:06

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 部署到 github pages 脚本
+# 错误时终止脚本
+set -e
+
+echo '删除打包文件夹'
+rm -rf public
+
+echo '打包。diray主题'
+hugo -t diary
+
+echo 'push网站源码'
+git init
+git add -A
+
+# Commit changes.
+msg="modify site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push -f git@github.com:syaofox/syaofox.github.io.git hug_blog
