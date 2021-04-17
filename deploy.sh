6cf3daa357cd7a4ef8c5db5aaf6a664d3b80cca7
syaofox <syaofox@gmai>
2021-04-17 22:04:00 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # 部署到 github pages 脚本
 # 错误时终止脚本
-set -e
-git pull
-echo '删除打包文件夹'
-rm -rf public
 
-echo '打包。diray主题'
-# hugo -t diary
-hugo
+# set -e
+# git pull
+# echo '删除打包文件夹'
+# rm -rf public
 
-echo 'push网站源码'
-#git init
-git add -A
+# echo '打包。diray主题'
+# # hugo -t diary
+# hugo
 
-# Commit changes.
-msg="modify site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+# echo 'push网站源码'
+# #git init
+# git add -A
 
-git push 
+# # Commit changes.
+# msg="modify site `date`"
+# if [ $# -eq 1 ]
+#   then msg="$1"
+# fi
+# git commit -m "$msg"
+
+# git push 
